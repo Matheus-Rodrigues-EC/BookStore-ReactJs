e8bf640d8a94b10067e8c24d1de7d707934f7148
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-04-27 15:48:18 -0300

    Created Project base for development

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import styled from "styled-components"
+import SignInPage from "./pages/SignInPage"
+import SignUpPage from "./pages/SignUpPage"
+
+export default function App() {
+  return (
+    <PagesContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/cadastro" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </PagesContainer>
+  )
+}
+
+const PagesContainer = styled.main`
+  background-color: #33FF33;
+  width: calc(100vw - 50px);
+  max-height: 100vh;
+  padding: 25px;
+`
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+import { BiExit } from "react-icons/bi"
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai"
+
+export default function HomePage() {
+    return (
+        null
+    )
+}
